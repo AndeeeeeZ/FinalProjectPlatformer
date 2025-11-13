b3480d66acdb20eb6761bb96f55dd2bbb5908263
Andi <zhuandeee@gmail.com>
2025-11-13 11:33:13 -0500

    Added variable jump height and variable gravity
    Now the longer the player presses the jump button, the higher they jump until they reached the limit.
    The variable gravity cause player to fall faster

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,16 @@ using UnityEngine.InputSystem.Controls;
 
 public class PlayerMovement : MonoBehaviour
 {
+
     [SerializeField]
     private float horizontalSpeed, jumpForce;
+
+    [SerializeField]
+    private float regularGravity, fallGravity;
+
+
     private PlatformerActions input;
-    private Rigidbody2D rb; 
+    private Rigidbody2D rb;
     private float horizontalMove;
 
     private void Awake()
@@ -19,12 +25,17 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         horizontalMove = 0f;
-        rb = GetComponent<Rigidbody2D>(); 
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = regularGravity;
     }
 
     private void Update()
     {
-        rb.velocity = new Vector2(horizontalMove * horizontalSpeed, rb.velocity.y); 
+        rb.velocity = new Vector2(horizontalMove * horizontalSpeed, rb.velocity.y);
+        if (rb.velocity.y < 0f)
+            rb.gravityScale = fallGravity;
+        else
+            rb.gravityScale = regularGravity;
     }
 
     private void OnEnable()
@@ -32,7 +43,8 @@ public class PlayerMovement : MonoBehaviour
         input.Enable();
         input.Player.Move.performed += OnMovePerformed;
         input.Player.Move.canceled += OnMoveCanceled;
-        input.Player.Jump.performed += OnJump; 
+        input.Player.Jump.performed += OnJumpPerformed;
+        input.Player.Jump.canceled += OnJumpCanceled;
     }
 
     private void OnDisable()
@@ -40,13 +52,13 @@ public class PlayerMovement : MonoBehaviour
         input.Disable();
         input.Player.Move.performed -= OnMovePerformed;
         input.Player.Move.canceled -= OnMoveCanceled;
-        input.Player.Jump.performed -= OnJump; 
+        input.Player.Jump.performed -= OnJumpPerformed;
+        input.Player.Jump.canceled -= OnJumpCanceled;
     }
 
     private void OnMovePerformed(InputAction.CallbackContext context)
     {
         horizontalMove = context.ReadValue<float>();
-        Debug.Log(horizontalMove);
     }
 
     private void OnMoveCanceled(InputAction.CallbackContext context)
@@ -54,8 +66,19 @@ public class PlayerMovement : MonoBehaviour
         horizontalMove = 0f;
     }
 
-    private void OnJump(InputAction.CallbackContext context)
+    private void OnJumpPerformed(InputAction.CallbackContext context)
     {
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); 
+        rb.gravityScale = regularGravity;
+        // Prevent falling velocity from reducing jump height
+        if (rb.velocity.y < 0f)
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void OnJumpCanceled(InputAction.CallbackContext context)
+    {
+        // Variable jump height
+        if (rb.velocity.y > 0f)
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y / 2f);
     }
 }
